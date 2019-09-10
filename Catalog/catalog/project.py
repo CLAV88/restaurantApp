ae00e30fdec64fd269b990cd8b7f9ecc64a94819
CLAV88 <cbissett88@gmail.com>
2019-09-10 08:51:40 -0400

    added flask-cors to venv

--- Catalog/catalog/project.py
+++ Catalog/catalog/project.py
@@ -1,6 +1,6 @@
 #!/usr/bin/venv python2.7
 from flask import Flask, render_template, request, redirect, jsonify, url_for, flash
-
+from flask_cors import CORS
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem, User
